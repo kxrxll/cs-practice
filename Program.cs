f58645d7d19868567a9c1a09ef650f05969a4bf9
kxrxll <kxrxll@kxrxlls-MacBook-Air.local>
2023-10-28 12:34:29 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using static Sortings;
+
+Sortings sortings = new Sortings();
+int[] array1 = { 1, 22, 44, 78, 5, 3, 18 };
+sortings.ShellSorting(array1).ToList().ForEach(i => Console.WriteLine(i.ToString()));
+int[] array2 = { 1, 22, 44, 78, 5, 3, 18 };
+sortings.BogoSorting(array2).ToList().ForEach(i => Console.WriteLine(i.ToString()));
+int[] array3 = { 1, 22, 44, 78, 5, 3, 18 };
+sortings.BubbleSort(array3).ToList().ForEach(i => Console.WriteLine(i.ToString()));
